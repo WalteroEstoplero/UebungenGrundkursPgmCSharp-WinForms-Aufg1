a4641809909954337a2631357a4572faffe70c32
Waltero Estoplero <waltser@web.de>
2019-11-18 18:47:29 +0100

    first commit

---
+++ UebungenGrundkursPgmCSharp-WinForms-Aufg1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace UebungenGrundkursPgmCSharp_WinForms_Aufg1
+{
+    partial class Form_Aufgabe1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_read = new System.Windows.Forms.Button();
+            this.TBx_file = new System.Windows.Forms.TextBox();
+            this.Btn_clear = new System.Windows.Forms.Button();
+            this.Btn_copy = new System.Windows.Forms.Button();
+            this.TBx_Kopie = new System.Windows.Forms.TextBox();
+            this.Btn_Verschl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_read
+            // 
+            this.Btn_read.Location = new System.Drawing.Point(45, 104);
+            this.Btn_read.Name = "Btn_read";
+            this.Btn_read.Size = new System.Drawing.Size(349, 56);
+            this.Btn_read.TabIndex = 0;
+            this.Btn_read.Text = "read file";
+            this.Btn_read.UseVisualStyleBackColor = true;
+            this.Btn_read.Click += new System.EventHandler(this.Btn_Read_Click);
+            // 
+            // TBx_file
+            // 
+            this.TBx_file.Location = new System.Drawing.Point(45, 56);
+            this.TBx_file.Name = "TBx_file";
+            this.TBx_file.Size = new System.Drawing.Size(717, 20);
+            this.TBx_file.TabIndex = 2;
+            // 
+            // Btn_clear
+            // 
+            this.Btn_clear.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Btn_clear.Location = new System.Drawing.Point(413, 104);
+            this.Btn_clear.Name = "Btn_clear";
+            this.Btn_clear.Size = new System.Drawing.Size(349, 56);
+            this.Btn_clear.TabIndex = 3;
+            this.Btn_clear.Text = "clear";
+            this.Btn_clear.UseVisualStyleBackColor = false;
+            this.Btn_clear.Click += new System.EventHandler(this.Btn_clear_Click);
+            // 
+            // Btn_copy
+            // 
+            this.Btn_copy.Location = new System.Drawing.Point(243, 191);
+            this.Btn_copy.Name = "Btn_copy";
+            this.Btn_copy.Size = new System.Drawing.Size(151, 56);
+            this.Btn_copy.TabIndex = 4;
+            this.Btn_copy.Text = "copy";
+            this.Btn_copy.UseVisualStyleBackColor = true;
+            this.Btn_copy.Click += new System.EventHandler(this.Btn_copy_Click);
+            // 
+            // TBx_Kopie
+            // 
+            this.TBx_Kopie.Location = new System.Drawing.Point(45, 191);
+            this.TBx_Kopie.Name = "TBx_Kopie";
+            this.TBx_Kopie.Size = new System.Drawing.Size(179, 20);
+            this.TBx_Kopie.TabIndex = 5;
+            // 
+            // Btn_Verschl
+            // 
+            this.Btn_Verschl.Location = new System.Drawing.Point(413, 189);
+            this.Btn_Verschl.Name = "Btn_Verschl";
+            this.Btn_Verschl.Size = new System.Drawing.Size(151, 60);
+            this.Btn_Verschl.TabIndex = 6;
+            this.Btn_Verschl.Text = "verschlüsseln";
+            this.Btn_Verschl.UseVisualStyleBackColor = true;
+            this.Btn_Verschl.Click += new System.EventHandler(this.Btn_Verschl_Click);
+            // 
+            // Form_Aufgabe1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 354);
+            this.Controls.Add(this.Btn_Verschl);
+            this.Controls.Add(this.TBx_Kopie);
+            this.Controls.Add(this.Btn_copy);
+            this.Controls.Add(this.Btn_clear);
+            this.Controls.Add(this.TBx_file);
+            this.Controls.Add(this.Btn_read);
+            this.Name = "Form_Aufgabe1";
+            this.Text = "Aufgabe1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_read;
+        private System.Windows.Forms.TextBox TBx_file;
+        private System.Windows.Forms.Button Btn_clear;
+        private System.Windows.Forms.Button Btn_copy;
+        private System.Windows.Forms.TextBox TBx_Kopie;
+        private System.Windows.Forms.Button Btn_Verschl;
+    }
+}
+
---
+++ UebungenGrundkursPgmCSharp-WinForms-Aufg1/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace UebungenGrundkursPgmCSharp_WinForms_Aufg1
+{
+    public partial class Form_Aufgabe1 : Form
+    {
+        string filename = "C:\\Temp\\Inhalt.txt";
+
+        public Form_Aufgabe1()
+        {
+            InitializeComponent();
+
+            Btn_read.Width = 349;
+            Btn_read.Height = 56;
+            Btn_read.Left = 45;
+            Btn_read.Top = 104;
+            Btn_read.BackColor = System.Drawing.Color.GreenYellow;
+
+            Btn_clear.Width = 349;
+            Btn_clear.Height = 56;
+            Btn_clear.Left = 413;
+            Btn_clear.Top = 104;
+            Btn_clear.BackColor = System.Drawing.Color.Blue;
+
+        }
+ //   }
+
+        private void Btn_Read_Click(object sender, EventArgs e)
+        {
+            int[] eingaben = new int[100];       //
+            string ausgabe = "";
+
+            if (!(File.Exists(filename)))
+            {
+                MessageBox.Show("Datei nicht gefunden !");
+            }
+            else
+            {
+                // Aus der Datei lesen        
+                System.IO.FileStream lesen = new System.IO.FileStream(filename, System.IO.FileMode.Open);
+                
+                int i = 0;                          // 
+                eingaben[0] = 0;
+                do
+                {
+                    if (eingaben[i] != -1)
+                    {
+                        eingaben[i] = lesen.ReadByte();
+                        ausgabe += (char)eingaben[i]; 
+                    }
+                    i++;
+                }
+                while (eingaben[i-1] != -1); 
+                lesen.Close();
+                TBx_file.Text = ausgabe;
+            }
+        }
+
+        private void Btn_clear_Click(object sender, EventArgs e)
+        {
+            TBx_file.Text = "";
+        }
+
+        private void Btn_copy_Click(object sender, EventArgs e)
+        {
+            string[] kopien;
+            string kopiename = "C:\\Temp\\" + TBx_Kopie.Text + ".txt";
+            if (File.Exists(kopiename)) { File.Delete(kopiename); }
+            File.Copy(filename, kopiename);
+            if (File.Exists(kopiename) && TBx_Kopie.Text != "")
+            {
+                kopien = File.ReadAllLines(kopiename);
+                foreach (string s in kopien)
+                {
+                    Console.WriteLine(s);
+                }
+                MessageBox.Show("Der Inhalt wurde in die Datei " + kopiename + " kopiert.");
+            }
+            else
+            {
+                MessageBox.Show("ERROR: Datei " + kopiename + " konnte nicht erstellt werden!");
+            }
+        }
+
+        private void Btn_Verschl_Click(object sender, EventArgs e)
+        {
+            int anz = 100;
+            int[] eingaben = new int[anz];       //
+            int[] verschl = new int[anz];
+            string ausgabe = "";
+
+            if (!(File.Exists(filename)))
+            {
+                MessageBox.Show("Datei nicht gefunden !");
+            }
+            else
+            {
+                // Aus der Datei lesen        
+                System.IO.FileStream lesen = new System.IO.FileStream(filename, System.IO.FileMode.Open);
+                int len = 0;
+                for (int i = 1; eingaben[i-1] != -1; i++)
+                {
+                    eingaben[i] = lesen.ReadByte();
+                    verschl[i] = eingaben[i] + 1;      // Die Verschlüsselung
+                    ausgabe += (char)(verschl[i]/*eingaben[i]*/);
+                    len = i;
+                }
+                
+                lesen.Close();
+                TBx_file.Text = ausgabe;
+                string fileverschl = "C:\\Temp\\Verschluessel.txt";
+
+                // Den verschlüsselten Text speichern
+                System.IO.FileStream schreiben = new System.IO.FileStream
+                    (fileverschl, System.IO.FileMode.Create);
+                for (int i = 1; i < len; i++)
+                {
+                    schreiben.WriteByte(Convert.ToByte(verschl[i]));
+                }
+                schreiben.Close();
+                if (File.Exists(fileverschl))
+                {
+                    MessageBox.Show("Die Verschlüsselung wurde kopiert !");
+                }
+                else
+                {
+                    MessageBox.Show("ERROR: Verschlüsselung wurde nicht gespeichert !");
+                }
+            }
+        }
+    }
+}
